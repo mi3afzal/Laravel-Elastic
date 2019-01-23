a320b9588d26dd4d3a71129ce571526fb753c8d9
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2019-01-23 05:25:47 +0500

    create nested-object mapping for relational data of users and articles inside Article model. create data indexing inside root router. create search route and figure out search string inside nested objects.

--- app/Article.php
+++ app/Article.php
@@ -1,12 +1,62 @@
 <?php
 
 namespace App;
+use Elasticquent\ElasticquentTrait;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
-{	
+{
+	use ElasticquentTrait;
+	
     protected $fillable = ['user_id', 'title', 'body', 'tags'];
+	
+	protected $mappingProperties = array(
+        'user_id' => [
+          'type' => 'text',
+          "analyzer" => "standard",
+        ],
+		'title' => [
+          'type' => 'text',
+          "analyzer" => "standard",
+        ],
+        'body' => [
+          'type' => 'text',
+          "analyzer" => "standard",
+        ],
+        'tags' => [
+          'type' => 'text',
+          "analyzer" => "standard",
+        ],
+		'user' => [
+			"type" => "nested",
+			"properties" => [
+				'name' => [
+					'type' => 'text',
+					"analyzer" => "standard",
+				],
+				'email' => [
+					'type' => 'text',
+					"analyzer" => "standard",
+				]
+			]
+		]
+     );
+	
+	public function getIndexDocumentData()
+    {
+        return array(
+            'id'	=> $this->id,
+            'user_id'	=> $this->user_id,
+            'title'	=> $this->title,
+            'body'	=> $this->body,
+            'tags'	=> $this->tags,
+            'user'  => array(
+				'name' 	=> $this->user->name,
+				'email' => $this->user->email
+			)
+        );
+    }
 	
 	public function user()
     {
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,73 @@
 |
 */
 
-Route::get('/welcome', function () {
+use App\Article;
+use App\User;
+
+Route::get('/', function () {
+		
+    //Article::deleteIndex();		
+    Article::createIndex($shards = null, $replicas = null);
+
+    //Article::deleteMapping();
+    Article::putMapping($ignoreConflicts = true);
+    //Article::rebuildMapping();
+
+    Article::addAllToIndex();
+
     return view('welcome');
 });
+
+
+Route::get('/search', function() {
+	//$articles = Article::search('a');
+    //$articles = Article::searchByQuery(['match' => ['title' => 'Sed']]); // working
+    //$articles = Article::searchByQuery(['match' => ['body' => 'a']]); // working
+    //$articles = Article::searchByQuery(['match' => ['nested' => ['user' => ['email' => 'susan88@example.net']]]]); // not work
+	
+	
+    $articles = Article::complexSearch(
+		["body" => ["query" => ["bool" => ["must" => [
+				["match" => ["title" => "aut"]],
+				["nested" => [
+					"path" => "user",
+					"query" => ["bool" => ["must" => [
+						["match" => ["user.name" => "PhD"]]
+					]]]
+				]]
+		]]]]]
+	);
+	
+    return $articles;
+});
+
+
+/*
+		["body" => ["query" => ["bool" => ["must" => [
+				["match" => ["title" => "aut"]],
+				["nested" => [
+					"path" => "user",
+					"query" => ["bool" => ["must" => [
+						["match" => ["user.name" => "PhD"]]
+					]]]
+				]]
+		]]]]]
+
+
+
+		["body" => ["query" => ["bool" => ["must" => [
+				["match" => ["title" => "Quae"]]
+		]]]]]
+
+
+
+		["body" => ["query" => ["bool" => ["must" => [
+				["nested" => [
+					"path" => "user",
+					"query" => ["bool" => ["must" => [
+						["match" => ["user.name" => "PhD"]]
+					]]]
+				]]
+		]]]]]
+*/
 
