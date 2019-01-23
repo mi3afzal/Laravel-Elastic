fa3741f520bfa9f181b7f74ff25a2847f4b618a6
Muhammad Irfan Afzal <mi3afzal@gmail.com>
2019-01-23 05:05:17 +0500

    Install Laravel and configure it. Add Elasticquent to project and configure it with running elastic search server.

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+	
+    public function run()
+    {
+		$this->command->call('migrate:fresh');
+        //$this->call(UsersTableSeeder::class);
+		$this->call(ArticleTableSeeder::class);
+    }
+}
